8565e15386721d156fd0cf773c2222037890ef32
dhruv7504 <139530447+dhruv7504@users.noreply.github.com>
2024-06-14 15:39:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch("https://reqres.in/api/users?page=2")
+    .then(res=>res.json())
+    .then(result=>{
+        output=''
+        for(let i=0;i<result["data"].length;i++){
+            user=result["data"][i]
+            output+=`<tr>
+            <td>${user.id}</td>
+             <td>${user.email}</td>
+             <td>${user.first_name}</td>
+             <td>${user.last_name}</td>
+              <td><td><img src="${user.avatar}"></td></td>
+        </tr>`}
+        console.table(result["data"])
+        document.getElementById("records").innerHTML=output
+
+    })
